14616ea2294d87db148852a724e00d43f38ba35b
mirenguerra <miren.guerra.sanz@gmail.com>
2019-06-27 20:21:18 +0200

    Add styles to Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './styles.scss';
-import Menu from '../Menu/index';
-import AboutMe from '../AboutMe/index';
+import React from "react";
+import "./styles.scss";
+import Menu from "../Menu/index";
+import AboutMe from "../AboutMe/index";
 
 function Home() {
   return (
     <div className="Home">
-      <h1>Home</h1>
-     <Menu />
-     <AboutMe />
+      <div className="Home__photo" />
+      <div className="Home__text-wrapper">
+        <h1 className="Home__title">Miren Guerra</h1>
+        <div className="Home__bar" />
+        <h2 className="Home__subtitle">Front-end developer</h2>
+      </div>
+      <Menu />
+      <AboutMe />
     </div>
   );
 }
