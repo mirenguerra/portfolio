539ba35068b18866e09ddecd91f77b4f5dec435d
mirenguerra <miren.guerra.sanz@gmail.com>
2019-07-09 16:09:53 +0200

    Change json and link correctly projects cover images

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.scss";
-import projects from "../../data/projects.json";
+import projects from "../../data/projects.js";
 import ProjectCard from '../ProjectCard/index';
 
 
