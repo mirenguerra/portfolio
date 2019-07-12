5bbf3f2125b1a18b25b0c2c1bb002618767d1659
mirenguerra <miren.guerra.sanz@gmail.com>
2019-07-12 11:45:54 +0200

    Create description about me

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -10,7 +10,26 @@ const AboutMe = props => {
     <div className="AboutMe">
       <img className="AboutMe__photo" src={MirenProfile} alt="Foto de Miren" />
       <h1 className="AboutMe__title">¡Hola, soy Miren!</h1>
-      <p className="AboutMe__description"> Soy...</p>
+      <h2 className="AboutMe__subtitle">¿Te cuento algo sobre mí?</h2>
+      <p className="AboutMe__description">
+        Estudié Ingeniería Técnica en Obras Públicas y trabajé durante años en
+        temas de movilidad y accesibilidad en entornos urbanos. Aunque me
+        gustaba mucho el trabajo, hace unos meses sentí la necesidad de
+        reinventarme y buscando enfrentarme a nuevos desafíos, decidí
+        convertirme en desarrolladora front-end. Gracias a Adalab he aprendido a
+        programar desde cero y a descubrir cuánto mola plantar cara a un reto
+        nuevo cada vez y conseguir comérmelo con patatas.
+      </p>
+      <h2 className="AboutMe__subtitle">¿Algo más?</h2>
+      <p className="AboutMe__description">
+        Me encantan los puzzles, los juegos de mesa y las manualidades. Cuando
+        tengo un rato libre me gusta crear mis propios proyectos DIY.
+      </p>
+      <h2 className="AboutMe__subtitle">¿Un secreto?</h2>
+      <p className="AboutMe__description">
+        Mi truco para relajarme es doblar calcetines, y cuanto más coloridos
+        sean, ¡mejor!.
+      </p>
       <h2 className="AboutMe__skills-title">Skills</h2>
       <ul className="AboutMe__skills-list">
         {skills.map(item => {
