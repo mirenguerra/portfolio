cf66e482be5d7f6fa0e54db39b28b10c21f77d9f
mirenguerra <miren.guerra.sanz@gmail.com>
2019-07-17 19:17:05 +0200

    Create docs

---
+++ docs/precache-manifest.31cb3a4214a5832b9d71eb5b19f7ff06.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373b3071a9353b9f242278439069172a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f85727c8a7207434635",
+    "url": "./static/css/main.6174abd0.chunk.css"
+  },
+  {
+    "revision": "18ed459a4ac45d624004",
+    "url": "./static/js/2.7b8b027a.chunk.js"
+  },
+  {
+    "revision": "7f85727c8a7207434635",
+    "url": "./static/js/main.114df084.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "05abd5edd5efbea2002e99bafff2a91c",
+    "url": "./static/media/AdivinaElNumero_cover.05abd5ed.png"
+  },
+  {
+    "revision": "095f8a02faea31ebb1dcd753ef48db26",
+    "url": "./static/media/AnonymousServer_cover.095f8a02.png"
+  },
+  {
+    "revision": "d4def0da6ec704425c0849227c5a4031",
+    "url": "./static/media/AwesomeProfileCardsReact_cover.d4def0da.png"
+  },
+  {
+    "revision": "815577ba4500b1390b0adf3951d25733",
+    "url": "./static/media/AwesomeProfileCards_cover.815577ba.png"
+  },
+  {
+    "revision": "7eae1ecd03cb03db5528417dbe612ee5",
+    "url": "./static/media/BmindHolidays_cover.7eae1ecd.png"
+  },
+  {
+    "revision": "2eab323fd194ff120f62e48b74a88f38",
+    "url": "./static/media/HarryPotterCharacters_cover.2eab323f.png"
+  },
+  {
+    "revision": "2d572b5ff40e42c1d7a4a6f05437bc4b",
+    "url": "./static/media/LifeCalendar_cover.2d572b5f.png"
+  },
+  {
+    "revision": "240b9ede8e6e62f83d8574c2397914dc",
+    "url": "./static/media/Miren-profile.240b9ede.jpg"
+  },
+  {
+    "revision": "66fc37cc44dd73323a375e60b472cb54",
+    "url": "./static/media/Miren.66fc37cc.jpg"
+  },
+  {
+    "revision": "f24a93500de09542c682897a0ed51df3",
+    "url": "./static/media/PokemonPokedex_cover.f24a9350.png"
+  },
+  {
+    "revision": "97f5a7d100c1638921e1f5639b644cf7",
+    "url": "./static/media/RockandCode_cover.97f5a7d1.png"
+  },
+  {
+    "revision": "87f29bee0451ffd3e7840d8a6865eeb6",
+    "url": "./static/media/agile-scrum.87f29bee.png"
+  },
+  {
+    "revision": "dd22b47d86f6eed2d87f2574b93aac87",
+    "url": "./static/media/background.dd22b47d.png"
+  },
+  {
+    "revision": "8bbf86e91218cc9bd71c03e1b884fa5e",
+    "url": "./static/media/bitbucket.8bbf86e9.png"
+  },
+  {
+    "revision": "bd476acffb357dec2552f772a02d16bb",
+    "url": "./static/media/bootstrap.bd476acf.png"
+  },
+  {
+    "revision": "c64430b2948cadf3e67a1a01b8fb1f78",
+    "url": "./static/media/css3.c64430b2.svg"
+  },
+  {
+    "revision": "5ee25cf90f975b4496448ff85b51362f",
+    "url": "./static/media/github.5ee25cf9.svg"
+  },
+  {
+    "revision": "5159ea36523919e57ee1b46bde9988cd",
+    "url": "./static/media/gulp.5159ea36.svg"
+  },
+  {
+    "revision": "be4ba3308529e2c5c3c015d6980100d1",
+    "url": "./static/media/home.be4ba330.svg"
+  },
+  {
+    "revision": "c83aaf3930796623cdbf82ca002d7005",
+    "url": "./static/media/html5.c83aaf39.svg"
+  },
+  {
+    "revision": "c2ebbccac59700b620e6d723ddc309de",
+    "url": "./static/media/ico-close.c2ebbcca.svg"
+  },
+  {
+    "revision": "61f6f66db7f32f9fc468e082e164c38f",
+    "url": "./static/media/ico-menu.61f6f66d.svg"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "./static/media/javascript.0cded3a3.png"
+  },
+  {
+    "revision": "921b41f8ff9600c42c7b8552ef876ad4",
+    "url": "./static/media/linkedin.921b41f8.svg"
+  },
+  {
+    "revision": "5f611998573357db5e2275125f29eadd",
+    "url": "./static/media/mail.5f611998.svg"
+  },
+  {
+    "revision": "84d44aff3af7aab4ce4495c3de9c2e64",
+    "url": "./static/media/react.84d44aff.png"
+  },
+  {
+    "revision": "ca5b9d80af2c6ff6731807dfc8a93701",
+    "url": "./static/media/restapi.ca5b9d80.png"
+  },
+  {
+    "revision": "73e24896289a49f3365faf66faec2f65",
+    "url": "./static/media/sass.73e24896.png"
+  },
+  {
+    "revision": "94ed2aab90612ce035578613dd521b83",
+    "url": "./static/media/sketch.94ed2aab.png"
+  },
+  {
+    "revision": "1ce6bc2de452d5db12c42fde54d84d03",
+    "url": "./static/media/trello.1ce6bc2d.svg"
+  },
+  {
+    "revision": "217ca08a5312cfdbf240d43e5b74de0a",
+    "url": "./static/media/twitter.217ca08a.svg"
+  },
+  {
+    "revision": "389f119bfd38b4eda8f3bc2cd1020bcd",
+    "url": "./static/media/vscode.389f119b.svg"
+  },
+  {
+    "revision": "c160c891de18871086809760e62d891e",
+    "url": "./static/media/zeplin.c160c891.png"
+  }
+]);
